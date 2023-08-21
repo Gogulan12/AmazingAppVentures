3862712c2f6838130ff9a055a6eed48930c6f2c3
Gogulan12 <gogulanravi12@gmail.com>
2023-08-21 18:27:21 -0400

    updated background colours

--- src/Components/AboutSection/About.js
+++ src/Components/AboutSection/About.js
@@ -6,22 +6,24 @@ import aboutimage from "../../Assets/aboutsection.svg";
 
 export default function About() {
   return (
-    <div className="aboutSection">
-      <div className="aboutImage">
-        <img src={aboutimage} alt="" />
-      </div>
-      <div className="aboutContent">
-        <h2>Who are we?</h2>
-        <p>
-          We are a team of highly-skilled facilitators with over 20 years of
-          experience in designing and executing interactive, fun and engaging
-          programs and events. It is in this spirit that we aspire to bring
-          unique, mobile and creative experiences and solutions to both private
-          vendors and the general public alike. It is our applications’
-          abilities to be completely customized and branded for every client’s
-          needs and locales that makes these programs truly Amazing
-          AppVentures™!
-        </p>
+    <div className="aboutsectioncontainer">
+      <div className="aboutSection">
+        <div className="aboutImage">
+          <img src={aboutimage} alt="" />
+        </div>
+        <div className="aboutContent">
+          <h2>Who are we?</h2>
+          <p>
+            We are a team of highly-skilled facilitators with over 20 years of
+            experience in designing and executing interactive, fun and engaging
+            programs and events. It is in this spirit that we aspire to bring
+            unique, mobile and creative experiences and solutions to both
+            private vendors and the general public alike. It is our
+            applications’ abilities to be completely customized and branded for
+            every client’s needs and locales that makes these programs truly
+            Amazing AppVentures™!
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/Components/NavigationSection/Navigation.js
+++ src/Components/NavigationSection/Navigation.js
@@ -5,26 +5,28 @@ import logo from "../../Assets/logo/logo.png";
 
 export default function Navigation() {
   return (
-    <div className="navigation">
-      <div className="logo">
-        {/* <p>logo</p> */}
-        <img src={logo} alt="logo" />
-      </div>
-      <div className="menu">
-        <ul>
-          <li>
-            <p>Capbilities</p>
-          </li>
-          <li>
-            <p>Opportunities</p>
-          </li>
-          <li>
-            <p>FAQ</p>
-          </li>
-        </ul>
-      </div>
-      <div className="download">
-        <button>Download</button>
+    <div className="navigationsection">
+      <div className="navigation">
+        <div className="logo">
+          {/* <p>logo</p> */}
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="menu">
+          <ul>
+            <li>
+              <p>Capbilities</p>
+            </li>
+            <li>
+              <p>Opportunities</p>
+            </li>
+            <li>
+              <p>FAQ</p>
+            </li>
+          </ul>
+        </div>
+        <div className="download">
+          <button>Download</button>
+        </div>
       </div>
     </div>
   );
