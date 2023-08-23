88298221f173d2c29209b2dc540979e624be642d
Gogulan12 <gogulanravi12@gmail.com>
2023-08-23 17:43:56 -0400

    updated brands section

--- src/Components/BrandSection/Brands.js
+++ src/Components/BrandSection/Brands.js
@@ -1,8 +1,4 @@
 import React from "react";
-// import scaveng from "../../Assets/brands/ScavengEARTH.jpg";
-// import seekqr from "../../Assets/brands/SeekQR-Icon.jpg";
-// import sightseekr from "../../Assets/brands/SightSeekr-Icon1.jpg";
-// import quizquest from "../../Assets/brands/QuizQuest-Icon.jpg";
 
 import wickedhunt from "../../Assets/brands/WICKEDHUNTSLOGO.png";
 import racing from "../../Assets/brands/AppMazingRace.png";
@@ -13,67 +9,73 @@ import "./Brands.css";
 
 export default function Brands() {
   return (
-    <div className="Brandsection">
-      <h2>OUR IN-HOUSE BRANDS</h2>
-      <div className="grid-container">
-        <div className="brand grid-item">
-          <div className="brandimageContainer">
-            <img src={wickedhunt} alt="" />
+    <div className="BrandsectionContainer">
+      <div className="Brandsection">
+        <h2>OUR IN-HOUSE BRANDS</h2>
+        <div className="grid-container">
+          <div className="brand grid-item">
+            <div className="brandimageContainer">
+              <div className="brandimage">
+                <img src={wickedhunt} alt="" />
+              </div>
+            </div>
+            <div className="brandContentContainer">
+              <h3>Wicked Hunts</h3>
+              <p>
+                Offering venues and properties four interactive mobile games for
+                team building or entertainment. These real-time games feature
+                GPS tracking, chat, and customization, allowing global play and
+                private previews on demand.
+              </p>
+            </div>
           </div>
-          <div className="brandContentContainer">
-            <h3>Wicked Hunts</h3>
-            <p>
-              Offering venues and properties four interactive mobile games for
-              team building or entertainment, including ScavengEARTH, Custom
-              Races, CSI-themed Investigations, and Agent 777. These real-time
-              games feature GPS tracking, chat, and customization, allowing
-              global play and private previews on demand.
-            </p>
+          <div className="brand grid-item">
+            <div className="brandimageContainer">
+              <div className="brandimage">
+                <img src={hunt} alt="" />
+              </div>
+            </div>
+            <div className="brandContentContainer">
+              <h3>AppMazing Hunt</h3>
+              <p>
+                Offering venues and properties four interactive mobile games for
+                team building or entertainment. These real-time games feature
+                GPS tracking, chat, and customization, allowing global play and
+                private previews on demand.
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="brand grid-item">
-          <div className="brandimageContainer">
-            <img src={hunt} alt="" />
+          <div className="brand grid-item">
+            <div className="brandimageContainer">
+              <div className="brandimage">
+                <img src={racing} alt="" />
+              </div>
+            </div>
+            <div className="brandContentContainer">
+              <h3>AppMazing Race</h3>
+              <p>
+                Offering venues and properties four interactive mobile games for
+                team building or entertainment. These real-time games feature
+                GPS tracking, chat, and customization, allowing global play and
+                private previews on demand.
+              </p>
+            </div>
           </div>
-          <div className="brandContentContainer">
-            <h3>AppMazing Hunt</h3>
-            <p>
-              Offering venues and properties four interactive mobile games for
-              team building or entertainment, including ScavengEARTH, Custom
-              Races, CSI-themed Investigations, and Agent 777. These real-time
-              games feature GPS tracking, chat, and customization, allowing
-              global play and private previews on demand.
-            </p>
-          </div>
-        </div>
-        <div className="brand grid-item">
-          <div className="brandimageContainer">
-            <img src={racing} alt="" />
-          </div>
-          <div className="brandContentContainer">
-            <h3>AppMazing Race</h3>
-            <p>
-              Offering venues and properties four interactive mobile games for
-              team building or entertainment, including ScavengEARTH, Custom
-              Races, CSI-themed Investigations, and Agent 777. These real-time
-              games feature GPS tracking, chat, and customization, allowing
-              global play and private previews on demand.
-            </p>
-          </div>
-        </div>
-        <div className="brand grid-item">
-          <div className="brandimageContainer">
-            <img src={agent} alt="" />
-          </div>
-          <div className="brandContentContainer">
-            <h3>Agent 777</h3>
-            <p>
-              Offering venues and properties four interactive mobile games for
-              team building or entertainment, including ScavengEARTH, Custom
-              Races, CSI-themed Investigations, and Agent 777. These real-time
-              games feature GPS tracking, chat, and customization, allowing
-              global play and private previews on demand.
-            </p>
+          <div className="brand grid-item">
+            <div className="brandimageContainer">
+              <div className="brandimage">
+                <img src={agent} alt="" />
+              </div>
+            </div>
+            <div className="brandContentContainer">
+              <h3>Agent 777</h3>
+              <p>
+                Offering venues and properties four interactive mobile games for
+                team building or entertainment. These real-time games feature
+                GPS tracking, chat, and customization, allowing global play and
+                private previews on demand.
+              </p>
+            </div>
           </div>
         </div>
       </div>
