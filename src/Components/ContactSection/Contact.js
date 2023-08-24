a14a2a9c81ea468dd2b3a35443d3fa6a88842e72
Gogulan12 <gogulanravi12@gmail.com>
2023-08-24 14:36:30 -0400

    updated contact section

--- src/Components/ContactSection/Contact.js
+++ src/Components/ContactSection/Contact.js
@@ -1,7 +1,9 @@
 import React from "react";
 import "./Contact.css"; // Create this CSS file for styling
 
-import phones from "../../Assets/phones.jpg";
+// import phones from "../../Assets/phones.jpg";
+// import phones from "../../Assets/phones-removebg-preview.png";
+import phones from "../../Assets/peopleholdingphones.png";
 
 function Contact() {
   return (
@@ -11,11 +13,11 @@ function Contact() {
           <h2>Contact Us</h2>
           <form>
             <label htmlFor="name">Name:</label>
-            <input type="text" id="name" name="name" />
+            <input type="text" id="name" name="name" required />
             <label htmlFor="email">Email:</label>
-            <input type="email" id="email" name="email" />
+            <input type="email" id="email" name="email" required />
             <label htmlFor="message">Message:</label>
-            <textarea id="message" name="message" rows="4"></textarea>
+            <textarea id="message" name="message" rows="4" required></textarea>
             <button type="submit">Submit</button>
           </form>
         </div>
@@ -33,12 +35,15 @@ function Contact() {
           <img src={phones} alt="" />
         </div>
         <div className="liveDemoText">
-          <h2>Let’s schedule a live demo</h2>
+          <h2>
+            Let’s schedule a <br />
+            live demo
+          </h2>
           <p>
             Let's have a chat and let us introduce you the future of corporate
             game platforms.
           </p>
-          <button>Schedule a call</button>
+          <button>Schedule a Call</button>
         </div>
       </section>
     </div>
