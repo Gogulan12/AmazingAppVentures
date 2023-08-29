92d418841fe938624aa486f68c212666dbc0826a
Gogulan12 <gogulanravi12@gmail.com>
2023-08-29 14:50:53 -0400

    updated footer and added terms and conditions modal

--- src/Components/FooterSection/Footer.js
+++ src/Components/FooterSection/Footer.js
@@ -1,13 +1,32 @@
-import React from "react";
+// import React from "react";
+import React, { useState } from "react";
 
 import "./Footer.css";
 
+import TermsModal from "../Modal/Modal";
+
 export default function Footer() {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
   return (
     <div>
       <footer className="footer">
-        <div className="footer-content">
-          <p>&copy; 2023 Amazing AppVentures. All rights reserved.</p>
+        <div className="footercontentcontainer">
+          <div className="footer-content">
+            <p>&copy; 2023 Amazing AppVentures. All rights reserved.</p>
+          </div>
+          <a href="#" onClick={openModal}>
+            View Terms and Conditions
+          </a>
+          <TermsModal isOpen={modalIsOpen} closeModal={closeModal} />
         </div>
       </footer>
     </div>
